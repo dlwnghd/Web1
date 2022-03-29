0c2608e80e3c4b20d203a8b6d41b5c167fe5d88f
Lee Juhong <alion0513@naver.com>
2022-03-29 16:30:58 +0900

    Seond Commit

---
+++ java/workspace/day02/src/day02/PrintTest.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class PrintTest {
+   public static void main(String[] args) {
+      
+      // 주석
+      // 1. 소스코드에 설명글을 달 때
+      // 2. 지금 당장 사용하지 않는 코드를 지우고 싶지 않을 때
+      
+      // 주석의 종류
+      /*
+       *   1. 한 줄 주석 : ctrl + /
+       * 2. 범위 주석 : ctrl + shift + /, 범위 주석 해제 : ctrl + shift + \
+       */
+      
+//      이름을 출력하는 부분
+      System.out.print("지우개");
+//      나이를 출력하는 부분
+      System.out.print("20살");
+   }
+}
---
+++ java/workspace/day02/src/day02/PrintTest2.java
@@ -0,0 +1,9 @@
+package day02;
+
+public class PrintTest2 {
+
+	public static void main(String[] args) {
+		int 왕뚜껑 = 1500;
+		System.out.println(왕뚜껑+"원");
+	}
+}
