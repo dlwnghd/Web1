821d051479cc9203911a748f652d5ea31b4ffd6a
Lee Juhong <alion0513@naver.com>
2022-05-09 21:51:43 +0900

    2022-05-09

---
+++ js/workspace/day08/task02/test.js
@@ -0,0 +1,29 @@
+let test = "안녕하세요";
+console.log(charAt('일'));
+
+// 내 풀이
+        // const next = document.querySelector("div.next");
+        // const prev = document.querySelector("div.prev");
+
+        // document
+        //     .getElementById("next")
+        //     .onclick = getNext;
+        // document
+        //     .getElementById("prev")
+        //     .onclick = getPrev;
+
+        // function getNext() {
+        //     count++;
+        //     if (count == 6) {
+        //         count = 0;
+        //     }
+        //     banner.style.transform = "translate(-" + count * 90 + "vw)";
+        // }
+
+        // function getPrev() {
+        //     count--;
+        //     if (count < 0) {
+        //         count = 5;
+        //     }
+        //     banner.style.transform = "translate(-" + count * 90 + "vw)";
+        // }
